7a08a0c205cc54e0cd62e8c24dffc60a74d27c26
Bao Nguyen <baonguyen130402@gmail.com>
2024-08-28 02:27:07 +0700

    feat(desktop/index.js): auto separate desktop layout

--- src/components/desktop/index.tsx
+++ src/components/desktop/index.tsx
@@ -17,109 +17,49 @@ const IconsArr = [
     title: "Edge",
     icon: <Icons.msEdge className="size-12 justify-self-center" />,
   },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  },
-  {
-    title: "",
-    icon: "",
-  }
 ];
 
+function separateDesktopLayout(
+  icons: any = [{ title: "", icon: "" }],
+  row: number = 9,
+  column: number = 18,
+) {
+  const { title, icon } = icons;
+
+  const desktopLayout = Array
+    .from(Array(row), () =>
+      new Array(column)
+        .fill({ icon: "", title: "" }));
+
+  if (title !== "" && icon !== "") {
+    icons.forEach((icon: any, id: number) => {
+      desktopLayout[id][0] = icon
+    })
+  }
+
+  return desktopLayout;
+}
+
 export function Desktop() {
-  const [parent, icons] = useDragAndDrop<HTMLUListElement, any>(IconsArr, {
-    group: "parent",
-    plugins: [swap()],
-  });
+  const array = separateDesktopLayout(IconsArr);
+
+  const [parent, rows] = useDragAndDrop<HTMLUListElement, any>(
+    array,
+    {
+      group: "parent",
+      plugins: [swap()],
+    },
+  );
 
   return (
     <ul
       className="w-screen h-[calc(100%-3rem)] bg-transparent p-2.5 grid grid-rows-9 grid-cols-18 gap-y-8 gap-x-2.5"
       ref={parent}
     >
-      {icons.map((icon: any) => (
-        <Icon key={icon.title} title={icon.title} icon={icon.icon} />
+      {rows.map((row: any) => (
+        row.map((item: any) => (
+          <Icon key={item.title} title={item.title} icon={item.icon} />
+        ))
       ))}
     </ul>
   );
