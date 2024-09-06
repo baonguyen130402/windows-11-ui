052c70735d2e11dde49396589dd1ab692e17ef68
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-06 17:07:32 +0700

    add theme-provider.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { MainLayout } from "./components/mainLayout";
 import ModalProvider from "./lib/contexts/ModalContext";
+import { ThemeProvider } from "./lib/theme/theme-provider";
 
 function App() {
   return (
-    <ModalProvider>
-      <MainLayout />
-    </ModalProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModalProvider>
+        <MainLayout />
+      </ModalProvider>
+    </ThemeProvider>
   );
 }
 
