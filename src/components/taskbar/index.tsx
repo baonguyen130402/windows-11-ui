ca21f9e760afc3934cfaec213d1a6336850b4ab0
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-11 17:49:54 +0700

    Feat(src/components/taskbar/weather.tsx): add weather component to taskbar

--- src/components/taskbar/index.tsx
+++ src/components/taskbar/index.tsx
@@ -1,14 +1,16 @@
 import { AppsPin } from "./appsPin";
-import { DateTime } from "./dateTime";
 import { HomeBtn } from "./homeBtn";
+import { DateTime } from "./dateTime";
 import { SearchBtn } from "./searchBtn";
 import { SystemIcons } from "./systemIcons";
 import { SystemIconTray } from "./systemIconTray";
+import { Weather } from "./weather";
 
 export function Taskbar() {
   return (
     <section className="grid grid-cols-4 gap-x-2 items-center fixed bottom-0 left-0 right-0 py-0.5 px-2.5 backdrop-blur-2xl bg-[rgba(0,0,0,.45)] w-screen h-12 border-t border-slate-600">
-      <div className="col-start-1 w-[270px]">
+      <div className="col-start-1 w-[270px] flex">
+        <Weather />
       </div>
       <div className="flex items-center justify-self-center col-start-2 col-span-2 gap-x-1">
         <HomeBtn />
---
+++ src/components/taskbar/weather.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { getWeather } from "../../lib/helper";
+import { Icons } from "../../lib/icons";
+
+interface IWeather {
+  temperature: number;
+  weatherType: string;
+}
+
+export function Weather() {
+  const [weather, setWeather] = useState<IWeather>({
+    temperature: 0,
+    weatherType: "",
+  });
+
+  useEffect(() => {
+    (async () => {
+      const data: any = await getWeather();
+      setWeather(data);
+    })();
+  }, []);
+
+  return (
+    <section className="flex items-center gap-2 hover:bg-gray-400 py-1 px-2 rounded-[0.25rem]">
+      <Icons.cloudRainy className="size-8" />
+      <div className="flex flex-col">
+        <span className="text-white text-xs">{weather.temperature} °C</span>
+        <span className="text-white text-xs">{weather.weatherType}</span>
+      </div>
+    </section>
+  );
+}
