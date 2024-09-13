7c1ad17b6ba0ad2ca661dc9ed901cd9b05457cbb
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-13 23:24:42 +0700

    feat(systemIcons.tsx): set brightness

--- src/components/mainLayout.tsx
+++ src/components/mainLayout.tsx
@@ -1,15 +1,22 @@
 import { Taskbar } from "./taskbar";
 import Wallpaper from "../assets/wallpaper.jpg";
 import { Desktop } from "./desktop";
+import { useContext } from "react";
+import { BrightnessContext } from "../lib/contexts/BrightnessContext";
 
 export function MainLayout() {
+  const { brightness } = useContext(BrightnessContext);
+  console.log(brightness);
+
   return (
     <main
       className="relative w-screen h-screen bg-fixed bg-cover p-0 m-0"
       style={{ backgroundImage: `url(${Wallpaper})` }}
     >
+      <div className="absolute bg-black" style={{ opacity: brightness / 100 }}>
         <Desktop />
         <Taskbar />
+      </div>
     </main>
   );
 }
