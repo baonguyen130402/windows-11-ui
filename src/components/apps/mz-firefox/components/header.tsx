9d8bdc06ef77fb3a790febc809f4880df36aecb9
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-19 15:14:18 +0700

    feat(firefox): firefox can draggable on desktop layout, can search like real browser

--- src/components/apps/mz-firefox/components/header.tsx
+++ src/components/apps/mz-firefox/components/header.tsx
@@ -6,23 +6,25 @@ import { MzFirefoxContext } from "../../../../lib/contexts/MzFirefoxContext";
 export function Header() {
   const { setMzFirefoxOpening } = useContext(MzFirefoxContext);
   return (
-    <div className="w-full h-10 flex justify-between items-center px-2 bg-neutral-900">
-      <div className="w-1/5 flex items-center gap-2 ml-8">
-        <Tab />
-        <Icons.plus className="size-3 fill-white" />
-      </div>
-      <div className="flex items-center justify-around w-[100px]">
-        <div className="p-2 hover:bg-gray-400 rounded-[0.25rem]">
-          <Icons.windowMinimize className="size-4 fill-white" />
+    <div className="title-bar">
+      <div className="w-full h-10 flex justify-between items-center px-2 bg-neutral-900">
+        <div className="w-1/5 flex items-center gap-2 ml-8">
+          <Tab />
+          <Icons.plus className="size-3 fill-white" />
         </div>
-        <div className="p-2 hover:bg-gray-400 rounded-[0.25rem]">
-          <Icons.windowMaximize className="size-4 fill-white" />
-        </div>
-        <div
-          onClick={() => setMzFirefoxOpening(false)}
-          className="p-2 hover:bg-red-400 rounded-[0.25rem]"
-        >
-          <Icons.effacer className="size-4 fill-white" />
+        <div className="flex items-center justify-around w-[100px]">
+          <div className="p-2 hover:bg-gray-400 rounded-[0.25rem]">
+            <Icons.windowMinimize className="size-4 fill-white" />
+          </div>
+          <div className="p-2 hover:bg-gray-400 rounded-[0.25rem]">
+            <Icons.windowMaximize className="size-4 fill-white" />
+          </div>
+          <div
+            onClick={() => setMzFirefoxOpening(false)}
+            className="p-2 hover:bg-red-400 rounded-[0.25rem]"
+          >
+            <Icons.effacer className="size-4 fill-white" />
+          </div>
         </div>
       </div>
     </div>
