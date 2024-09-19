12653cf395c2fa660cefb875f1ae755465775c22
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-19 20:07:25 +0700

    styling navbar

--- src/components/apps/ms-edge/components/navbar.tsx
+++ src/components/apps/ms-edge/components/navbar.tsx
@@ -3,7 +3,7 @@ import { Icons } from "../../../../lib/icons";
 
 export function Navbar() {
   return (
-    <nav className="grid grid-cols-12 w-full h-12 gap-2 px-2 py-1">
+    <nav className="grid grid-cols-12 w-full h-12 gap-2 px-2 py-2">
       <div className="col-span-2 justify-self-start flex items-center gap-4 justify-center">
         <div className="p-1">
           <Icons.arrowLeft className="size-5 fill-white" />
@@ -15,12 +15,13 @@ export function Navbar() {
           <Icons.refresh className="size-5 fill-transparent" />
         </div>
       </div>
-      <div className="col-span-7 border border-gray-500 flex items-center px-2 bg-neutral-900">
+      <div className="col-span-7 border border-gray-500 flex items-center px-2 bg-neutral-900 rounded-full">
         <Icons.search className="size-5 fill-white" />
         <Input
           placeholder="Search with Google or enter address"
           className="px-2 border-none"
         />
+        <Icons.bookmark className="size-5" />
       </div>
       <div className="justify-self-end col-span-3 self-center pr-2">
         <Icons.burgerMenu className="size-5 fill-white" />
