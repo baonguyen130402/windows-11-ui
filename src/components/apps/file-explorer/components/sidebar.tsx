e80e463d8ffd28c155ee620144c2447ed5d158ec
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-25 05:05:02 +0700

    ref(sidebar): change svg icons to png icons and use data from ../lib/data/file-explorer

--- src/components/apps/file-explorer/components/sidebar.tsx
+++ src/components/apps/file-explorer/components/sidebar.tsx
@@ -1,42 +1,16 @@
 import { useDragAndDrop } from "@formkit/drag-and-drop/react";
+
 import { Icons } from "../../../../lib/icons";
 import { animations } from "@formkit/drag-and-drop";
-
-const defaultApps = [
-  {
-    title: "Desktop",
-    icon: <Icons.desktopFolder className="size-5 mr-2" />,
-  },
-  {
-    title: "Documents",
-    icon: <Icons.documentFolder className="size-5 mr-2" />,
-  },
-  {
-    title: "Downloads",
-    icon: <Icons.downloadsFolder className="size-5 mr-2" />,
-  },
-  {
-    title: "Music",
-    icon: <Icons.musicFolder className="size-5 mr-2" />,
-  },
-  {
-    title: "Pictures",
-    icon: <Icons.picturesFolder className="size-5 mr-2" />,
-  },
-  {
-    title: "Videos",
-    icon: <Icons.videosFolder className="size-5 mr-2" />,
-  },
-  {
-    title: "Project",
-    icon: <Icons.folder className="size-5 mr-2" />,
-  },
-];
+import { defaultAppsInSideBar } from "../../../../lib/data/fileExplorer";
 
 export function Sidebar() {
-  const [parent, apps] = useDragAndDrop<HTMLUListElement, any>(defaultApps, {
-    plugins: [animations()],
-  });
+  const [parent, apps] = useDragAndDrop<HTMLUListElement, any>(
+    defaultAppsInSideBar,
+    {
+      plugins: [animations()],
+    },
+  );
 
   return (
     <div className="col-span-3 border-r border-gray-700 h-full px-2 pt-3">
@@ -45,25 +19,42 @@ export function Sidebar() {
         <span className="text-sm font-bold">Home</span>
       </div>
       <div className="flex items-center px-1 py-1">
-        <Icons.chevronRight className="size-4 mr-1.5 " />
-        <Icons.cloud className="size-5 fill-blue-400 mr-1.5" />
-        <span className="text-sm font-medium">Baonguyen - Personal</span>
+        <img
+          src="/images/file-explorer/gallery.png"
+          className="ml-5 size-5 fill-blue-400 mr-1.5"
+        />
+        <span className="text-sm font-medium">Gallery</span>
       </div>
       <ul ref={parent} className="border-b border-t border-gray-700 my-2 py-2">
-        {apps.map((app) => (
+        {apps.map((app, index) => (
           <li key={app.title} className="flex justify-between items-center">
             <div className="flex items-center px-4 ml-2 py-1">
               {app.icon}
               <span className="text-sm font-medium">{app.title}</span>
             </div>
-            <Icons.pin className="size-4 fill-gray-700" />
+            {index !== apps.length - 1
+              ? <Icons.pin className="size-4 fill-gray-700" />
+              : ""}
           </li>
         ))}
       </ul>
-      <div className="flex items-center px-1 py-1">
-        <Icons.chevronRight className="size-4 mr-1.5 " />
-        <Icons.thisPc className="size-5 fill-blue-400 mr-1.5" />
-        <span className="text-sm font-medium">This PC</span>
+      <div>
+        <div className="flex items-center px-1 py-1">
+          <Icons.chevronRight className="size-[12px] mr-1.5 " />
+          <img
+            src="/images/file-explorer/Computer.ico"
+            className="size-5 fill-blue-400 mr-1.5"
+          />
+          <span className="text-sm font-medium">This PC</span>
+        </div>
+        <div className="flex items-center px-1 py-1">
+          <Icons.chevronRight className="size-[12px] mr-1.5 " />
+          <img
+            src="/images/file-explorer/Network.ico"
+            className="size-5 fill-blue-400 mr-1.5"
+          />
+          <span className="text-sm font-medium">Network</span>
+        </div>
       </div>
     </div>
   );
