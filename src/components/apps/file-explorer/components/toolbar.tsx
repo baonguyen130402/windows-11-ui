e6aac98bd26249bdcb53b832db8bb7e5c024284d
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-25 05:06:12 +0700

    ref(toolbar): change svg icons to png icons and use data from ../lib/data/file-explorer

--- src/components/apps/file-explorer/components/toolbar.tsx
+++ src/components/apps/file-explorer/components/toolbar.tsx
@@ -1,28 +1,43 @@
+import {
+  optionsArrangeInToolbar,
+  optionsWithFileInToolbar,
+} from "../../../../lib/data/fileExplorer";
 import { Icons } from "../../../../lib/icons";
 
 export function Toolbar() {
   return (
     <div className="relative bg-[#191919] bg-blur-lg backdrop-filter h-14 flex items-center border-t border-b border-t-gray-700 border-b-gray-700">
-      <div className="flex items-center gap-2 px-2">
+      <div className="flex items-center gap-2 px-4">
         <Icons.plusCircle className="size-4 fill-[#999]" />
         <span className="text-[#999] text-sm font-semibold">New</span>
         <Icons.lineAngleUp className="rotate-180 size-2 fill-[#999]" />
       </div>
-      <div className="px-2 border-r border-l border-[#999]">
-        Something
-      </div>
-      <div className="flex items-center gap-2 px-2">
-        <Icons.view className="size-4 fill-[#999]" />
-        <span className="text-[#999] text-sm font-semibold">View</span>
-        <Icons.lineAngleUp className="rotate-180 size-2 fill-[#999]" />
-      </div>
-      <div className="flex items-center gap-2 px-2">
-        <Icons.filter className="size-4 fill-[#999]" />
-        <span className="text-[#999] text-sm font-semibold">Filter</span>
-        <Icons.lineAngleUp className="rotate-180 size-2 fill-[#999]" />
-      </div>
-      <div className="flex items-center gap-2 px-2">
-        <Icons.threeDots className="size-4 fill-[#999]" />
+      <ul className="px-4 border-r border-l border-[#999] flex items-center justify-between gap-4">
+        {optionsWithFileInToolbar.map((option, key) => (
+          <li
+            key={key}
+            className="hover:bg-[#626262] p-2 hover:rounded-[0.25rem]"
+          >
+            {option.icon}
+          </li>
+        ))}
+      </ul>
+      <ul className="flex items-center px-4 gap-2 border-r border-[#999]">
+        {optionsArrangeInToolbar.map((option, key) => (
+          <li
+            key={key}
+            className="flex items-center gap-2 px-2 hover:bg-[#626262] p-2 hover:rounded-[0.25rem]"
+          >
+            {option.icon}
+            <span className="text-[#999] text-sm font-semibold">
+              {option.title}
+            </span>
+            <Icons.lineAngleUp className="rotate-180 size-2 fill-[#999]" />
+          </li>
+        ))}
+      </ul>
+      <div className="flex items-center gap-4 px-4 hover:bg-[#626262] p-2 hover:rounded-[0.25rem] ml-4">
+        <img src="/images/options/dots.png" className="size-5 fill-[#999]" />
       </div>
     </div>
   );
