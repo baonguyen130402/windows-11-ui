8ad8c49aaf380862637690f499860ecb84761559
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-26 11:10:39 +0700

    fix(mainLayout): add function to gererate brightness unit

--- src/components/mainLayout.tsx
+++ src/components/mainLayout.tsx
@@ -3,12 +3,12 @@ import clsx from "clsx";
 import { Taskbar } from "./taskbar";
 import { Desktop } from "./desktop";
 import Wallpaper from "../assets/wallpaper.jpg";
-import { brightnessUnit } from "../lib/utilities/brightness";
+import { genarateBrightnessUnit } from "../lib/utilities/brightness";
 
 export function MainLayout() {
   const className = clsx(
-    "w-screen h-screen bg-fixed bg-cover p-0 m-0",
-    brightnessUnit,
+    "w-screen h-screen bg-fixed bg-cover p-0 m-0 overflow-hidden",
+    genarateBrightnessUnit(),
   );
 
   return (
