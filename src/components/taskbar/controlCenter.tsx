e7e992b174a0923f74e571e70d9491ce20afec7d
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-26 10:28:25 +0700

    update somethinggg

--- src/components/taskbar/controlCenter.tsx
+++ src/components/taskbar/controlCenter.tsx
@@ -13,7 +13,7 @@ export function Modal(props: any) {
   const { setBrightness } = useContext(BrightnessContext);
 
   const myClassName = clsx(
-    "transition-all fixed border border-slate-600 bg-gray-900 h-80 text-slate-400 rounded-[0.5rem] p-4",
+    "transition-all fixed border border-slate-600 bg-gray-900 h-80 text-slate-400 rounded-[0.5rem] p-6",
     {
       "right-2.5 w-[22rem] -bottom-80": !isOpen,
       "right-2.5 w-[22rem] bottom-[58px]": isOpen,
@@ -22,34 +22,36 @@ export function Modal(props: any) {
 
   return (
     <div className={myClassName}>
-      <ul className="grid grid-cols-3 grid-rows-2 gap-3">
+      <ul className="grid grid-cols-3 grid-rows-2 gap-5">
         {buttonInControlSystem.map((button, key) => (
           <li key={key} className="text-center group w-full">
-            <div className="bg-gray-700 w-[6.1rem] h-12 border border-gray-500 rounded-[0.25rem] flex items-center justify-center group-hover:bg-gray-600">
+            <div className="bg-gray-700 w-[5.8rem] h-12 border border-gray-500 rounded-[0.25rem] flex items-center justify-center group-hover:bg-gray-600">
               {button.icon}
             </div>
             <span className="tracking-wider text-sm">{button.title}</span>
           </li>
         ))}
       </ul>
-      <div className="flex items-center gap-2 mt-4">
+      <div className="flex items-center justify-between gap-2 mt-4">
         <Icons.speaker className="size-4" />
         <Slider
           defaultValue={[50]}
           max={100}
           step={1}
-          className="w-full"
+          className="w-4/5"
         />
+        <Icons.speaker className="size-4" />
       </div>
-      <div className="flex items-center gap-2 mt-4">
+      <div className="flex items-center justify-between gap-2 mt-4">
         <SunIcon className="size-4" />
         <Slider
           onValueChange={(value) => setBrightness(value[0])}
           defaultValue={[100]}
           max={200}
           step={10}
-          className="w-full"
+          className="w-4/5"
         />
+        <SunIcon className="size-4" />
       </div>
     </div>
   );
