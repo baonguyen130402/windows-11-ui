a5f6d80d3cd5ffe70584dc84d60559e9e3a56512
Bao Nguyen <baonguyen130402@gmail.com>
2024-09-29 15:16:22 +0700

    ref: add file miniView.tsx to show mini view of app

--- src/lib/utilities/miniView.tsx
+++ src/lib/utilities/miniView.tsx
@@ -1,16 +1,13 @@
+import { useContext } from "react";
 import { MsEdge } from "../../components/apps/ms-edge";
+import { MsEdgeContext } from "../contexts/MsEdgeContext";
 
 export function MiniView() {
+  const { msEdgeOpening } = useContext(MsEdgeContext);
+
   return (
-    <div className="absolute w-[130px] h-[150px] bg-[#1d1d1d] bottom-[55px] rounded-[0.3em] p-[0.5em] hidden group-hover:flex flex-col items-center gap-y-2">
-      <div className="flex items-center gap-2 w-full">
-        <img src="/images/apps/edge.png" className="size-6" />
-        <span className="line-clamp-1 text-sm w-full">
-          New tab - Profile 1 - Microsoft Edge
-        </span>
-      </div>
-      <div className="w-[114px] h-[120px] bg-white overflow-hidden">
-      </div>
+    <div className="absolute hidden w-[130px] h-[150px] bg-[#1d1d1d] bottom-[55px] rounded-[0.3em] p-[0.5em] group-hover:block flex-col items-center gap-y-2">
+      <MsEdge isOpen={msEdgeOpening} inMiniview={true} />
     </div>
   );
 }
