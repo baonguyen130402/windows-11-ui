2d4ca72ef9f5e3a4d46b2e60551e6c8f542bc694
Bao Nguyen <baonguyen130402@gmail.com>
2024-10-03 03:15:02 +0700

    ref(msEdge): use RenderIf

--- src/components/apps/ms-edge/index.tsx
+++ src/components/apps/ms-edge/index.tsx
@@ -6,6 +6,7 @@ import Draggable from "react-draggable";
 import { Header } from "./components/header";
 import { Navbar } from "./components/navbar";
 import { Bookmark } from "./components/bookmark";
+import { RenderIf } from "../../../lib/utilities/renderIf";
 
 export function MsEdge(props: any) {
   const { isOpen, isMinimize, isMaximize, inMiniview = false } = props;
@@ -23,36 +24,34 @@ export function MsEdge(props: any) {
   };
 
   return (
-    <>
-      {isOpen && (
-        <Draggable handle=".title-bar" nodeRef={explorerRef} bounds={bounds}>
-          <article
-            ref={explorerRef}
-            className={clsx(
-              "backdrop-filter backdrop-blur-lg rounded-[0.5rem] overflow-hidden absolute border border-[#2c3246]",
-              {
-                "hidden": isMinimize,
-                "w-screen h-screen rounded-none": isMaximize,
-                "block w-[72rem] h-[45rem]": isMinimize === false ||
-                  isMaximize === false,
-                "w-[114px] h-[120px] bg-white": inMiniview,
-              },
-            )}
-          >
-            <Header />
-            <div className="bg-[#3b3b3b]">
-              <Navbar />
-              <Bookmark />
-            </div>
-            <div className="content w-full h-full bg-[#202124]">
-              <iframe
-                src={homeUrl}
-                className="w-full h-full"
-              />
-            </div>
-          </article>
-        </Draggable>
-      )}
-    </>
+    <RenderIf isTrue={isOpen}>
+      <Draggable handle=".title-bar" nodeRef={explorerRef} bounds={bounds}>
+        <article
+          ref={explorerRef}
+          className={clsx(
+            "backdrop-filter backdrop-blur-lg rounded-[0.5rem] overflow-hidden absolute border border-[#2c3246]",
+            {
+              "hidden": isMinimize,
+              "w-screen h-screen rounded-none": isMaximize,
+              "block w-[72rem] h-[45rem]": isMinimize === false ||
+                isMaximize === false,
+              "w-[114px] h-[120px] bg-white": inMiniview,
+            },
+          )}
+        >
+          <Header />
+          <div className="bg-[#3b3b3b]">
+            <Navbar />
+            <Bookmark />
+          </div>
+          <div className="content w-full h-full bg-[#202124]">
+            <iframe
+              src={homeUrl}
+              className="w-full h-full"
+            />
+          </div>
+        </article>
+      </Draggable>
+    </RenderIf>
   );
 }
