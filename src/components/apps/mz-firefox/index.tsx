2e7f429e0299c23de771ab309c8e30ae0ddb899c
Bao Nguyen <baonguyen130402@gmail.com>
2024-10-03 02:30:12 +0700

    ref(mzFirefox): use RenderIf

--- src/components/apps/mz-firefox/index.tsx
+++ src/components/apps/mz-firefox/index.tsx
@@ -1,9 +1,12 @@
 import { useRef } from "react";
-import { Bookmark } from "./components/bookmark";
+
+import clsx from "clsx";
+import Draggable from "react-draggable";
+
 import { Header } from "./components/header";
 import { Navbar } from "./components/navbar";
-import Draggable from "react-draggable";
-import clsx from "clsx";
+import { Bookmark } from "./components/bookmark";
+import { RenderIf } from "../../../lib/utilities/renderIf";
 
 export function MzFirefox(props: any) {
   const { isOpen, isMinimize, isMaximize, inMiniview = false } = props;
@@ -21,36 +24,34 @@ export function MzFirefox(props: any) {
   };
 
   return (
-    <>
-      {isOpen && (
-        <Draggable handle=".title-bar" nodeRef={explorerRef} bounds={bounds}>
-          <article
-            ref={explorerRef}
-            className={clsx(
-              "rounded-[0.5rem] overflow-hidden border border-[#2c3246]",
-              {
-                "hidden": isMinimize,
-                "w-screen h-screen rounded-none": isMaximize,
-                "block w-[72rem] h-[45rem] bg-gray-900/80":
-                  isMinimize === false || isMaximize === false,
-                "w-[114px] h-[120px] bg-white": inMiniview,
-              },
-            )}
-          >
-            <Header />
-            <div className="bg-gray-700">
-              <Navbar />
-              <Bookmark />
-            </div>
-            <div className="content w-full h-full bg-[#202124]">
-              <iframe
-                src={homeUrl}
-                className="w-full h-full flex-grow"
-              />
-            </div>
-          </article>
-        </Draggable>
-      )}
-    </>
+    <RenderIf isTrue={isOpen}>
+      <Draggable handle=".title-bar" nodeRef={explorerRef} bounds={bounds}>
+        <article
+          ref={explorerRef}
+          className={clsx(
+            "rounded-[0.5rem] overflow-hidden border border-[#2c3246]",
+            {
+              "hidden": isMinimize,
+              "w-screen h-screen rounded-none": isMaximize,
+              "block w-[72rem] h-[45rem] bg-gray-900/80":
+                isMinimize === false || isMaximize === false,
+              "w-[114px] h-[120px] bg-white": inMiniview,
+            },
+          )}
+        >
+          <Header />
+          <div className="bg-gray-700">
+            <Navbar />
+            <Bookmark />
+          </div>
+          <div className="content w-full h-full bg-[#202124]">
+            <iframe
+              src={homeUrl}
+              className="w-full h-full flex-grow"
+            />
+          </div>
+        </article>
+      </Draggable>
+    </RenderIf>
   );
 }
