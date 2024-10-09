9172e7cc0b005520475146eae583ed6c504a8a40
Bao Nguyen <baonguyen130402@gmail.com>
2024-10-09 11:49:36 +0700

    fixed: click close button save latest positon of app

--- src/components/apps/file-explorer/components/header.tsx
+++ src/components/apps/file-explorer/components/header.tsx
@@ -8,28 +8,28 @@ export function Header() {
   );
 
   return (
-    <div className="title-bar">
-      <div className="w-full h-9 flex justify-between pl-2 bg-[#020d1d] relative">
+    <div className="w-full h-9 flex items-center pl-2 bg-[#020d1d] relative">
+      <div className="title-bar w-[calc(100%-132px)]">
         <Tab />
-        <div className="flex items-center w-[132px]">
-          <div
-            onClick={() => minimizeApp()}
-            className="px-4 py-3 hover:bg-gray-600"
-          >
-            <Icons.windowMinimize className="size-3 fill-white" />
-          </div>
-          <div
-            onClick={() => maximizeApp()}
-            className="px-4 py-3 hover:bg-gray-600"
-          >
-            <Icons.windowMaximize className="size-3 fill-white" />
-          </div>
-          <div
-            onClick={() => closeApp()}
-            className="px-4 py-3 hover:bg-red-600"
-          >
-            <Icons.effacer className="size-3 fill-white" />
-          </div>
+      </div>
+      <div className="flex items-center w-[132px]">
+        <div
+          onClick={() => minimizeApp()}
+          className="px-4 py-3 hover:bg-gray-600"
+        >
+          <Icons.windowMinimize className="size-3 fill-white" />
+        </div>
+        <div
+          onClick={() => maximizeApp()}
+          className="px-4 py-3 hover:bg-gray-600"
+        >
+          <Icons.windowMaximize className="size-3 fill-white" />
+        </div>
+        <div
+          onClick={() => closeApp()}
+          className="px-4 py-3 hover:bg-red-600"
+        >
+          <Icons.effacer className="size-3 fill-white" />
         </div>
       </div>
     </div>
