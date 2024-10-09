ab5d12ecfcc0641ebec5e8771fcd83db42b777fd
Bao Nguyen <baonguyen130402@gmail.com>
2024-10-09 23:17:11 +0700

    make usePosition more dynamic

--- src/lib/customHooks/index.tsx
+++ src/lib/customHooks/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export function usePosition() {
+export function usePosition(app: string) {
   const [latestPosition, setLatestPosition] = useState({
     x: 0,
     y: 0,
@@ -8,22 +8,30 @@ export function usePosition() {
 
   const getPositionFromLocalStorage = () => {
     try {
-      return JSON.parse(localStorage.getItem("latestPosition") || "");
+      return JSON.parse(localStorage.getItem(`latestPosition${app}`) || "");
     } catch (error) {
       return latestPosition;
     }
   };
 
   const position = getPositionFromLocalStorage();
-  const setPosition = (dragElement: any) =>
-    setLatestPosition({
-      x: dragElement.x,
-      y: dragElement.y,
-    });
+  const setPosition = (dragElement: any) => {
+    const { x: coordX, y: coordY } = dragElement;
+
+    if (coordX >= 0 && coordY >= 0) {
+      return setLatestPosition({
+        x: coordX,
+        y: coordY,
+      });
+    } else return setLatestPosition({ x: 0, y: 0 });
+  };
 
   useEffect(() => {
     if (latestPosition.x !== 0 || latestPosition.y !== 0) {
-      localStorage.setItem("latestPosition", JSON.stringify(latestPosition));
+      localStorage.setItem(
+        `latestPosition${app}`,
+        JSON.stringify(latestPosition),
+      );
     }
   }, [latestPosition.x, latestPosition.y]);
 
