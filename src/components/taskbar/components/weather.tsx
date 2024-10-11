deee49d11359a9cf22daba8bfdb9b83d72bcb68d
Bao Nguyen <baonguyen130402@gmail.com>
2024-10-11 13:41:43 +0700

    ref(weather): turn off getWeather()

--- src/components/taskbar/components/weather.tsx
+++ src/components/taskbar/components/weather.tsx
@@ -14,12 +14,12 @@ export function Weather() {
     weatherType: "",
   });
 
-  useEffect(() => {
-    (async () => {
-      const data: any = await getWeather();
-      setWeather(data);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const data: any = await getWeather();
+  //     setWeather(data);
+  //   })();
+  // }, []);
 
   return (
     <section className="flex items-center gap-2 hover:bg-gray-400 py-1 px-2 rounded-[0.25rem]">
