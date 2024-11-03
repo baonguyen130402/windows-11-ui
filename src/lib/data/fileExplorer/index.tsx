50870325ac413a1bcd3a5642f9f796c0aeeb3e45
baonguyexn <baonguyen130402@gmail.com>
2024-11-03 11:28:46 +0700

    fix(data/fileExplorer): not show icons

--- src/lib/data/fileExplorer/index.tsx
+++ src/lib/data/fileExplorer/index.tsx
@@ -1,30 +1,49 @@
-import { title } from "process";
-import { Icons } from "../../icons";
+import desktopFolder from "/images/file-explorer/Desktop.ico"
+import documentFolder from "/images/file-explorer/Documents.ico"
+import downloadFolder from "/images/file-explorer/Downloads.ico"
+import musicFolder from "/images/file-explorer/Music.ico"
+import pictureFolder from "/images/file-explorer/Photos.ico"
+import videoFolder from "/images/file-explorer/Videos.ico"
+import projectFolder from "/images/file-explorer/projects.png"
+
+import cutIcon from "/images/options/cut.png"
+import copyIcon from "/images/options/copy.png"
+import pasteIcon from "/images/options/paste.png"
+import renameIcon from "/images/options/rename.png"
+import shareIcon from "/images/options/share.png"
+import deleteIcon from "/images/options/delete.png"
+import sortIcon from "/images/options/sort.png"
+import viewIcon from "/images/options/view.png"
+import dotIcon from "/images/options/dots.png"
+
+import galleryIcon from "/images/file-explorer/gallery.png"
+import thisPcIcon from "/images/file-explorer/Computer.ico"
+import networkIcon from "/images/file-explorer/Network.ico"
 
 export const defaultAppsInSideBar = [
   {
     title: "Desktop",
     icon: (
-      <img src="/images/file-explorer/Desktop.ico" className="h-5 w-5 mr-2" />
+      <img src={desktopFolder} className="h-5 w-5 mr-2" />
     ),
   },
   {
     title: "Documents",
     icon: (
-      <img src="/images/file-explorer/Documents.ico" className="h-5 w-5 mr-2" />
+      <img src={documentFolder} className="h-5 w-5 mr-2" />
     ),
   },
   {
     title: "Downloads",
     icon: (
-      <img src="/images/file-explorer/Downloads.ico" className="h-5 w-5 mr-2" />
+      <img src={downloadFolder} className="h-5 w-5 mr-2" />
     ),
   },
   {
     title: "Music",
     icon: (
       <img
-        src="/images/file-explorer/Music.ico"
+        src={musicFolder}
         className="h-5 w-5 mr-2"
       />
     ),
@@ -32,56 +51,58 @@ export const defaultAppsInSideBar = [
   {
     title: "Pictures",
     icon: (
-      <img src="/images/file-explorer/Photos.ico" className="h-5 w-5 mr-2" />
+      <img src={pictureFolder} className="h-5 w-5 mr-2" />
     ),
   },
   {
     title: "Videos",
     icon: (
-      <img src="/images/file-explorer/Videos.ico" className="h-5 w-5 mr-2" />
+      <img src={videoFolder} className="h-5 w-5 mr-2" />
     ),
   },
   {
     title: "Project",
     icon: (
-      <img src="/images/file-explorer/projects.png" className="h-5 w-5 mr-2" />
+      <img src={projectFolder} className="h-5 w-5 mr-2" />
     ),
   },
-
-  //{
-  //  title: "Downloads",
-  //  icon: <img src="/images/apps/folder.png" className="h-5 w-5 mr-2" />,
-  //},
 ];
 
 export const optionsWithFileInToolbar = [
   {
-    icon: <img src="/images/options/cut.png" className="size-5" />,
+    icon: <img src={cutIcon} className="size-5" />,
   },
   {
-    icon: <img src="/images/options/copy.png" className="size-5" />,
+    icon: <img src={copyIcon} className="size-5" />,
   },
   {
-    icon: <img src="/images/options/paste.png" className="size-5 scale-150" />,
+    icon: <img src={pasteIcon} className="size-5 scale-150" />,
   },
   {
-    icon: <img src="/images/options/rename.png" className="size-5" />,
+    icon: <img src={renameIcon} className="size-5" />,
   },
   {
-    icon: <img src="/images/options/share.png" className="size-5" />,
+    icon: <img src={shareIcon} className="size-5" />,
   },
   {
-    icon: <img src="/images/options/delete.png" className="size-5" />,
+    icon: <img src={deleteIcon} className="size-5" />,
   },
 ];
 
 export const optionsArrangeInToolbar = [
   {
-    icon: <img src="/images/options/sort.png" className="size-5" />,
+    icon: <img src={sortIcon} className="size-5" />,
     title: "Sort",
   },
   {
-    icon: <img src="/images/options/view.png" className="size-5" />,
+    icon: <img src={viewIcon} className="size-5" />,
     title: "View",
   },
 ];
+
+export const other = {
+  dotIcon: <img src={dotIcon} className="size-5" />,
+  galleryIcon: <img src={galleryIcon} className="size-5 fill-blue-400 ml-5 mr-1.5" />,
+  networkIcon: <img src={networkIcon} className="size-5 fill-blue-400 mr-1.5" />,
+  thisPcIcon: <img src={thisPcIcon} className="size-5 fill-blue-400 mr-1.5" />,
+}
